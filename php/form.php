b7351aefcac9c471f7ada629af469b8fba11a1a7
Anna Lee <47875904+annaleee@users.noreply.github.com>
2021-11-24 03:35:39 -0500

    Add files via upload

--- php/form.php
+++ php/form.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
 
- You have posted <?php echo $_GET["suggestions"]; ?><br>
+ You have posted <?php echo $_POST["suggestions"]; ?><br>
  Thank you for your reponse, although I don't care about yor comment, but still thank you.
-
-</body>
-</html>
